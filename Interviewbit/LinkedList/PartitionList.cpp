5f1932e8bd78b8844db7e7cd4cf2292cdb2400a8
Himanshu Batra <hbatra21@gmail.com>
2019-12-13 11:27:08 +0530

    Create PartitionList.cpp

---
+++ Interviewbit/LinkedList/PartitionList.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::partition(ListNode* A, int B) {
+  /*  ListNode* temp=A;
+    deque<int>d;
+    while(temp){
+        if(temp->val==B) break;
+        temp=temp->next;
+    }
+    ListNode* curr=temp;
+    ListNode* prev=temp;
+    while(temp){
+        if(temp->val<B) {
+            d.push_back(temp->val);
+            prev->next=temp->next;
+            temp=temp->next;
+        }
+        else{
+        prev=temp;
+        temp=temp->next;
+        }
+    }
+    ListNode* head=A;
+    ListNode* post=A->next;
+    ListNode* p;
+    while(head!=curr){
+        p->val=d.front();
+        d.pop_front();
+        head->next=p;
+        p->next=post;
+        head=head->next;
+    }
+    return A;*/
+       ListNode* c1=NULL,*c2=NULL;
+    ListNode* l1=c1,*l2=c2;
+    ListNode* temp=A;
+    while(temp!=NULL){
+        if(temp->val<B){
+            if(c1==NULL){
+                c1=new ListNode(temp->val);
+                l1=c1;
+            }
+            else{
+                l1->next=new ListNode(temp->val);
+                l1=l1->next;
+            }
+        }
+        else{
+            if(c2==NULL){
+                c2=new ListNode(temp->val);
+                l2=c2;
+            }
+            else{
+                l2->next=new ListNode(temp->val);
+                l2=l2->next;
+            }
+        }
+        ListNode* x=temp;
+        temp=temp->next;
+        free(x);
+        A=temp;
+    }
+    if(c1==NULL) return c2;
+    l1->next=c2;
+    return c1;
+
+}
