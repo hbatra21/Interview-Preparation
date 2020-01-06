b175dcae27c5bf6e9b11ac20a6aaf61cdbddc2fd
Himanshu Batra <hbatra21@gmail.com>
2020-01-06 15:34:10 +0530

    Uisng StringStream

---
+++ Leetcode/OccuranceAfterBigram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> findOcurrences(string text, string first, string second) {
+        vector<string> v;
+        vector<string> ans;
+        stringstream ss(text);
+        string word;
+        while(ss >> word ) {
+            v.push_back(word);
+        }
+         for(int i = 0; i < v.size() - 2; i++) {
+            if(v[i] == first && v[i + 1] == second) {
+                ans.push_back(v[i + 2]);
+            }
+        }
+        return ans;
+    }
+};
