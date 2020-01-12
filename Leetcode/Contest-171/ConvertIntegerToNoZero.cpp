d632665bc7b2d637589ddad606e07c679a8be985
Himanshu Batra <hbatra21@gmail.com>
2020-01-12 12:47:28 +0530

    *APPROACH*

---
+++ Leetcode/Contest-171/ConvertIntegerToNoZero.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        for (int a = 1; a < n; ++a) {
+            int b = n - a;
+            if (to_string(a).find('0') == string::npos && to_string(b).find('0') == string::npos)
+                return {a, b};
+        }
+        return {}; // unreachable because at least one valid solution
+    }
+};
