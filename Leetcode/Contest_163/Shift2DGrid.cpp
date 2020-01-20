a503c79b0bdf87df49431ccc5b70f98878683633
Himanshu Batra <hbatra21@gmail.com>
2020-01-20 07:54:01 +0530

    2 Approaches**

--- Leetcode/Contest_163/Shift2DGrid.cpp
+++ Leetcode/Contest_163/Shift2DGrid.cpp
@@ -22,4 +22,29 @@ public:
         }
         return grid;
     }
+}; 
+
+
+// Approach 2
+class Solution {
+public:
+vector<vector> shiftGrid(vector<vector>& grid, int k) {
+    int n=grid.size(),m=grid[0].size(),temp;
+    while(k--){
+      int i=n-1,j=m-1;
+      temp=grid[i][j];
+    for(int t=1;t<n*m;t++)
+    if(j==0){
+        j=m-1;
+        grid[i][0]=grid[i-1][j];
+        i=i-1;
+    }
+    else{
+        grid[i][j]=grid[i][j-1];
+        j=j-1;
+    }
+    grid[0][0]=temp;   
+    }
+    return grid;
+}
 };
