5395e51afe54fcf7380d3e4fb01b55b8e778a879
Himanshu Batra <hbatra21@gmail.com>
2020-02-09 10:41:38 +0530

    * HARD *

--- Leetcode/Biweekly-Contest-19/JumpGame4.cpp
+++ Leetcode/Biweekly-Contest-19/JumpGame4.cpp
@@ -67,39 +67,49 @@ public:
 
 // BFS MODIFIED SOLUTION
 
-int minJumps(vector<int>& arr) {
-        queue<pair<int, int>> q;
-        unordered_map<int, vector<int>> mp;
-        for(int i = 0; i < arr.size(); i++)
-            mp[arr[i]].push_back(i);
-        int n = arr.size();
-        vector<int> vis(n, false);
-        q.push({ n - 1, 0 });
-        vis[n - 1] = 1;
-        int steps = 0;
-        while(!q.empty()) {
-            int sz = q.size();
-			int f = q.front().first;
-			int steps = q.front().second;
-			q.pop();
-			if(f == 0)
-				return steps;
-			if(f - 1 >= 0 && !vis[f - 1]) {
-				q.push({ f - 1, steps + 1 });
-				vis[f - 1] = true;
-			}
-			if(f + 1 < n && !vis[f + 1]) {
-				q.push({ f + 1, steps + 1 });
-				vis[f + 1] = true;
-			}
-			for(auto j : mp[arr[f]]) {
-				if(j == f)
-					continue;
-				if(!vis[j]) {
-					vis[j] = true;
-					q.push({ j, steps + 1 });
-				}
-			}
+constexpr int MAX_SIZE = 5e4;
+
+int minJumps(vector<int> const& arr) {
+  int const N = arr.size();
+
+  // Handle trivial case
+  if (N == 1)
+    return 0;
+
+  // Build inverse of arr (skip index 0)
+  unordered_map<int, vector<int>> inverse;
+  for (int i = 1; i < N; ++i)
+    inverse[arr[i]].push_back(i);
+
+  // BFS
+  queue<int> q;
+  q.push(0);
+  bitset<MAX_SIZE+1> visited;
+  visited[0] = true;
+  for (int result = 1; !q.empty(); ++result) {
+    int size = q.size();
+    while (size--) {
+      int i = q.front();
+      q.pop();
+
+      auto processNext = [&](int j){
+        if (j == N-1)
+          return true;
+        if (j >= 0 && j < N && !visited[j]) {
+          visited[j] = true;
+          q.push(j);
         }
-        return -1; 
+        return false;
+      };
+
+      if (processNext(i+1) || processNext(i-1))
+        return result;
+      for (int j : inverse[arr[i]])
+        if (j != i && processNext(j))
+          return result;
+      inverse[arr[i]].clear();
     }
+  }
+
+  assert(false);
+}
