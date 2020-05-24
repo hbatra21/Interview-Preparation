a1b8e10162043206d17b5aa3ab6626577d334e98
Himanshu Batra <hbatra21@gmail.com>
2020-05-24 18:56:17 +0530

    Create Template.cpp

---
+++ GFG_MUST_DO/Strings/Template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  LL long long
+#define F first
+#define S second
+#define PII pair<int,int>
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		
+	}
+}
