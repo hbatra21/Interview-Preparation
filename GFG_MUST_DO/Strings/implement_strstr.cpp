b848e1de153a3a16d07f4acb493031dc85a27eb7
Himanshu Batra <hbatra21@gmail.com>
2020-05-24 18:57:33 +0530

    Add files via upload

---
+++ GFG_MUST_DO/Strings/implement_strstr.cpp
@@ -0,0 +1,21 @@
+int strstr(string s, string x)
+{
+     for(int i=0;i<s.size();i++)
+     {
+         for(int j=0;j<x.size();j++)
+         {
+             if(i+j < s.size())
+             {
+                 if(x[j] != s[i+j])
+                    break;
+             }
+             else 
+             {
+                 break;
+             }
+             if(j == x.size() - 1)
+                return i;
+         }
+     }
+     return -1;
+}
